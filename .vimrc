46f55558e7671ea8feca897c225bbb45d142f089
Ivan Kovalenko <raintimeUnhandledEXcepti0n@gmail.com>
2026-05-15 16:04:51 +0200

    update

--- .vimrc
+++ .vimrc
@@ -1,18 +1,19 @@
 syntax on
-set number relativenumber
-set scrolloff=999
-set tabstop=8
-set shiftwidth=8
+set tabstop=4
+set shiftwidth=4
 set autoindent
 set expandtab
 set ruler
 set list
 set lcs=tab:>-,space:·,trail:~,extends:>,precedes:<,nbsp:␣
 set ignorecase smartcase
-set clipboard+=unnamedplus
+set clipboard+=unnamed
 cnoreabbrev t term
 cnoreabbrev ex Ex
-colorscheme default
-" set colorcolumn=80
-set bg=dark
 set noswapfile
+set guicursor=a:ver1
+nn j jzz
+nn k kzz
+set splitright
+set number
+colorscheme koehler
---
+++ init.lua
@@ -0,0 +1,25 @@
+vim.opt.number=true
+vim.opt.tabstop=4
+vim.opt.shiftwidth=4
+vim.opt.autoindent=true
+vim.opt.expandtab=true
+vim.opt.ruler=true
+vim.opt.list=true
+vim.opt.lcs="tab:>-,space:·,trail:~,extends:>,precedes:<,nbsp:␣"
+vim.opt.ignorecase=true
+vim.opt.smartcase=true
+vim.opt.clipboard="unnamedplus"
+vim.cmd("cabbrev t term")
+vim.cmd("cabbrev ex Ex")
+vim.cmd("noswapfile=on")
+vim.opt.guicursor="a:ver1"
+vim.cmd("nn j jzz")
+vim.cmd("nn k kzz")
+vim.opt.mousescroll="ver:1,hor:1"
+vim.opt.splitright=true
+vim.opt.cmdheight=0
+vim.cmd.swapfile=false
+vim.opt.sidescroll=1
+vim.opt.sidescrolloff=5
+-- vim.opt.background=dark
+vim.cmd("colorscheme vim")
